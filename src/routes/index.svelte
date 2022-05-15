b1449f0a3cee3a8f39e6be4e24aa66186c1519aa
Alex <alex@unixfy.net>
2022-05-15 19:25:03 +0000

    Add jumbotron to index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<h1 class="text-3xl font-display">Almost before we knew it!</h1>
+<div class="h-[calc(50vh)] flex bg-[url('/images/bg.webp')] bg-center bg-auto bg-grey-500">
+	<div class="h-100 m-auto text-center text-white drop-shadow-md">
+		<h1 class="font-display sm:text-3xl md:text-4xl lg:text-5xl text-2xl font-bold">
+			Welcome to Unixfy Docs
+		</h1>
+		<h2 class="font-display text-lg sm:text-xl md:text-2xl lg:text-3xl font-semibold mt-4">
+			What are you looking for today?
+		</h2>
+	</div>
+</div>
+
+<div class="ct">
+
+</div>
