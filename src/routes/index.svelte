71e07c721bc9b2cae1545b5d69209c1a6cc8ef3a
Alex <alex@unixfy.net>
2022-05-15 17:50:01 +0000

    Copy over the styles from unixfy/unixfy.net

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,3 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<h1 class="text-3xl font-display">Almost before we knew it!</h1>
