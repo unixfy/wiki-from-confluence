62cdf39d58c38e9dcafb0d40534a36a41de079b8
Alex <alex@unixfy.net>
2022-05-15 22:52:13 +0000

    Build out layout for wiki pages

---
+++ src/routes/page/[title]/__layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Sidenav from '$components/Sidenav.svelte';
+</script>
+
+<div class="page-wrapper">
+	<div class="ct">
+		<div class="grid sm:grid-cols-7 gap-8 mt-4">
+			<div class="col-span-2">
+				<Sidenav />
+			</div>
+			<div class="col-span-5">
+				<slot />
+			</div>
+		</div>
+	</div>
+</div>
