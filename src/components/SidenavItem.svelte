bef5a63c74601790a6aebfd54e6f9faa3430bc98
unixfy <alex@unixfy.net>
2022-06-15 00:09:18 -0400

    Figure out a really hacky way to check if nav children are active

--- src/components/SidenavItem.svelte
+++ src/components/SidenavItem.svelte
@@ -1,25 +1,46 @@
 <script>
-	import { page } from '$app/stores';
-	import {buildPageUrl} from "$lib/buildPageUrl";
+    import {page} from '$app/stores';
+    import {buildPageUrl} from "$lib/buildPageUrl";
 
-	export let data;
-	export let indent = 0;
+    export let data;
+    export let indent = 0;
 
-	let open = true;
+    // We need to make this reactive so the variable updates when pages switch :)
+    $: isActive = (data.id === $page.params.id);
+    // REALLY janky way to check if any of the children are active (and the item will be open if it is)
+    $: isChildrenActive = (JSON.stringify(data.children.page.results).includes($page.params.id));
+
+    let open = false;
+
+    function toggle() {
+        open = !open;
+    }
 </script>
 
-<a href="{buildPageUrl(data.title, data.id)}">
-	<div
-		style="padding-left: {4 + indent}px;"
-		class="py-1 hover:bg-gray-300 dark:hover:bg-slate-700"
-		class:font-bold={data.id === $page.params.id}
-	>
-		{data.title}
-	</div>
-</a>
+<div class="py-1 hover:bg-gray-300 dark:hover:bg-slate-700" style="padding-left: {4 + indent}px;"
+     class:bg-gray-300={isActive} class:dark:bg-slate-700={isActive}>
+    <span on:click={toggle}>
+        <!--    Show a different icon if there's children-->
+        {#if data.children.page.results.length > 0}
+        {#if (open || isChildrenActive)}
+            <i class="fas fa-chevron-down"></i>
+        {:else}
+            <i class="fas fa-chevron-right"></i>
+        {/if}
+    {:else}
+        <i class="far fa-file"></i>
+    {/if}
+    </span>
 
-{#if open && data.children.page.results !== []}
-	{#each data.children.page.results as child}
-		<svelte:self data={child} indent={indent + 8} />
-	{/each}
+    <a href="{buildPageUrl(data.title, data.id)}">
+        <span>
+            {data.title}
+        </span>
+    </a>
+</div>
+
+{#if open || isChildrenActive}
+    {#each data.children.page.results as child}
+        <svelte:self data={child} indent={indent + 8} />
+    {/each}
 {/if}
