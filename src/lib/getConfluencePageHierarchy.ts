cfc4c7b22e53c3dc8a9e8a1b2d8d6c7d1efa06b0
unixfy <alex@unixfy.net>
2022-06-14 20:06:16 -0400

    Properly source the hierarchy root page from stores too, oops!

--- src/lib/getConfluencePageHierarchy.ts
+++ src/lib/getConfluencePageHierarchy.ts
@@ -1,10 +1,10 @@
-import { atlassianTenantName } from "../stores";
+import { atlassianTenantName,spaceRootPage } from "../stores";
 import {get} from "svelte/store";
 
 export async function getConfluencePageHierarchy() {
     // Note that this is limited to 5 levels of hierarchy (increase levels by adding more children.page expands)
     const response = await fetch(
-        `https://corsproxy.unixfy.net/?https://${get(atlassianTenantName)}.atlassian.net/wiki/rest/api/content/4096009/child/page?expand=children.page.children.page.children.page.children.page`
+        `https://corsproxy.unixfy.net/?https://${get(atlassianTenantName)}.atlassian.net/wiki/rest/api/content/${get(spaceRootPage)}/child/page?expand=children.page.children.page.children.page.children.page`
     );
     return await response.json();
 }
